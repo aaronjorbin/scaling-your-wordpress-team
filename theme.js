e80740d922fb95c73adddbb0b63fab4e71710c3e
Aaron Jorbin <aaron@jorb.in>
2019-06-11 19:24:37 -0400

    initial commit

---
+++ theme.js
@@ -0,0 +1,11 @@
+export default {
+
+	font: '"Verlag Cond A", "Verlag Cond B"',
+	heading: {
+		fontWeight: 700,
+	},
+	pre: {
+		fontSize: '0.75em' 
+	},
+	googleFont: 'https://cloud.typography.com/7598912/612904/css/fonts.css',
+}
